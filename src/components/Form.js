e86c8686c7bc8c668a99adf6b024634a0e8c7a0f
yachuh <yachuh@gmail.com>
2022-06-29 09:53:16 +0800

    add Tailwind CSS styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,20 +3,21 @@ import React from "react";
 const Form = () => {
     return(
         <div>
-            <form>
+            <form className="flex flex-col space-y-2 w-1/3 p-10">
                 <label htmlFor="email">Email:
-                    <input type={`text`} name={`email`} id={`email`} placeholder={`Enter your email`}/>
+                    <input className="mt-1 mb-2 px-2 py-2 border rounded-lg block w-full focus:border-black" type={`text`} name={`email`} id={`email`} placeholder={`Enter your email`}/>
                 </label>
                 <label htmlFor="name">Name:
-                    <input type={`text`} name={`name`} id={`name`} placeholder={`Enter your name`}/>
+                    <input className="mt-1 mb-2 px-2 py-2 border rounded-lg block w-full focus:border-black" type={`text`} name={`name`} id={`name`} placeholder={`Enter your name`}/>
                 </label>
                 <label htmlFor="pwd">Password:
-                    <input type={`password`} name={`pwd`} id={`pwd`} placeholder={`Enter your password`}/>
+                    <input className="mt-1 mb-2 px-2 py-2 border rounded-lg block w-full focus:border-black" type={`password`} name={`pwd`} id={`pwd`} placeholder={`Enter your password`}/>
                 </label>
                 <label htmlFor="pwd2">Confirm password:
-                <input type={`password`} name={`pwd`} id={`pwd2`} placeholder={`Enter your password again`}/>
+                <input className="mt-1 mb-2 px-2 py-2 border rounded-lg block w-full focus:border-black" type={`password`} name={`pwd`} id={`pwd2`} placeholder={`Enter your password again`}/>
                 </label>
-                <input type={`button`} value={`Register Now`}></input>
+                <input className="bg-blue-500 text-white rounded-lg px-2 py-2 w-full cursor-pointer hover:bg-blue-700" type={`button`} value={`Register Now`}></input>
+                <a className="text-center" href="../../public/index.html">登入</a>
             </form>
         </div>
     );
